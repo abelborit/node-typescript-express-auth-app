2bd59704805f27e1042a275400327c779b963711
Abel Borit <abelboritguitton@gmail.com>
2024-09-18 17:01:28 -0500

    [add] CategoryRoutes & CategoryController

--- src/presentation/routes.ts
+++ src/presentation/routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { AuthRoutes } from "./auth/routes";
+import { CategoryRoutes } from "./category/routes";
 
 export class AppRoutes {
   /* aquí se utiliza static functions porque como no se hará inyección de dependencias entonces no sería necesario instanciar la clase AppRoutes y solo se coloca directo. También se están usando el get function para tener otra forma de realizar esta función, se podría realizar sin ese get (son solo diferentes formas de hacerlo) */
@@ -10,6 +11,7 @@ export class AppRoutes {
     /* este sería un middelware básicamente porque es una función que se va a ejecutar cuando pase la ruta por aquí en "/api/todos" */
     // router.use("/api/todos", TodosRoutes.routes); // ejemplo de lo que podría crearse a futuro para usar en este archivo de routes
     router.use("/api/auth", AuthRoutes.routes);
+    router.use("/api/categories", CategoryRoutes.routes);
 
     return router;
   }
