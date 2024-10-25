e1d86dde78d0ee98ba707d9c71b0d6bf87da58ea
Abel Borit <abelboritguitton@gmail.com>
2024-10-25 18:03:46 -0500

    [add] fileName & valid extensions

--- src/presentation/services/file-upload.service.ts
+++ src/presentation/services/file-upload.service.ts
@@ -1,10 +1,15 @@
 import { UploadedFile } from "express-fileupload";
 import fs from "fs";
 import path from "path";
+import { Uuid } from "../../config";
+import { CustomError } from "../../domain/errors/custom.error";
 
 export class FileUploadService {
   /* nuestro constructor será para poder hacer la inyección de dependencias en caso se necesite */
-  constructor() {}
+  constructor(
+    /* aquí no se necesita instanciar la clase Uuid porque "uuidv4" es un método estático que hemos creado */
+    private readonly uuid = Uuid.uuidv4
+  ) {}
 
   /* verificar si existe el folder donde se quiere guardar la imagen */
   private checkFolder(folderPath: string) {
@@ -22,15 +27,28 @@ export class FileUploadService {
     validExtensions: string[] = ["png", "jpg", "jpeg", "gif"]
   ) {
     try {
-      const fileExtension = file.mimetype.split("/").at(1); // para tomar la segunda posición por ejemplo image/jpeg -> jpeg
+      const fileExtension = file.mimetype.split("/").at(1) ?? ""; // para tomar la segunda posición por ejemplo image/jpeg -> jpeg y si no viene nada entonces que le coloque un string vacío
+
+      if (!validExtensions.includes(fileExtension)) {
+        throw CustomError.badRequest_400(
+          `Invalid extension: -- ${fileExtension} --, valid ones -- ${validExtensions} --`
+        );
+      }
+
       const destination = path.resolve(__dirname, "../../../", folder); // el __dirname es relativo a la carpeta en donde se está ejecutando este archivo de "file-upload.service.ts" y toda esta función hará que se tome el directorio relativo a "file-upload.service.ts", luego que suba 3 directorios y que tome el folder que viene el cual debería estar en el root de la aplicación
 
       this.checkFolder(destination);
 
+      const fileName = `${this.uuid()}` + "." + `${fileExtension}`;
+
       /* "mv" es una función que mueve el archivo a cualquier lugar que queramos */
-      file.mv(destination + "/" + "nombre-archivo." + fileExtension);
+      file.mv(destination + "/" + fileName);
+
+      return { fileName };
     } catch (error) {
       console.log("file-upload.service.ts", { error });
+
+      throw error;
     }
   }
 
